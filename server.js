e7d153808b6100eeeaac2885ef5a5a4b65c4bf63
sudeeepaa <155514292+sudeeepaa@users.noreply.github.com>
2025-08-14 11:45:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const DataStorage = require('./data/storage');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT; 
 
 const dataStorage = new DataStorage();
 
